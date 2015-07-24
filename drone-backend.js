dfae849980a46ecf7993ca1d7d231e92672f76b3
V3516 (Conference ID) <V3516@cam.ac.uk>
2015-07-24 13:05:50 +0100

    Drone

--- drone-backend.js
+++ drone-backend.js
@@ -12,11 +12,70 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",      // Combine with a second device to have more information
+        connection: "ardrone"
+    })
     .on("ready", fly);
     
 // Fly the bot
 function fly(robot) {
+    bot = robot;
+    bot.drone.config('general:navdata_demo', 'TRUE');
 
+    bot.nav.on("navdata", function(data) {
+        // console.log(data);
+    });
+
+    bot.nav.on("altitudeChange", function(data) {
+        console.log("Altitude:", data);
+        // Drone is higher than 1.5 meters up
+       // if (altitude > 1.5) {
+       //     bot.drone.land();
+       // }
+    });
+
+    bot.nav.on("batteryChange", function(data) {
+        console.log("Battery level:", data);
+    });
+
+    // Disable emergency setting if there was any
+    bot.drone.disableEmergency();
+    // Tell the drone it is lying horizontally
+    bot.drone.ftrim();
+
+    // Take off
+    bot.drone.takeoff();
+    after(5*1000, function() {
+        bot.drone.front(0.5);
+    });
+
+    after(10*1000, function() {
+        bot.drone.right(0.5);
+        after(12*1000, function() {
+            bot.drone.right(0);
+        });
+    });
+
+    after(14*1000, function() {
+        bot.drone.down(0.3);
+        after(16*1000, function() {
+            bot.drone.down(0);
+        });
+    });
+
+    after(18*1000, function() {
+        bot.drone.front(0.5);
+    });
+
+    after(25*1000, function() {
+        bot.drone.land();
+    });
+
+    after(
+        27*1000, function() {
+        bot.drone.stop();
+    });
 }
 
 Cylon.start();
--- drone-frontend.js
+++ drone-frontend.js
@@ -1,20 +1,46 @@
 function init() {
-    tracker = initTracker("#example");
+    var tracker = initTracker("#example");
     tracking.track("#example .drone", tracker);
 }
 
 function initTracker(element) {
     // Initialise a color tracker
     var tracker = new tracking.ColorTracker();
-    
+
     TrackerUtils.startTrackingColors(tracker);
 
     // Whenever there is a new color detected, mark them
     tracker.on('track', function(event) {
+        markColors(event.data, element);
         console.log(event.data);
     });
 
     return tracker;
+}
+
+function markColors(colors, element) {
+    // Do the marking
+    var canvas = $(element + ' .canvas').get(0);
+    var context = canvas.getContext('2d');
+
+    context.clearRect(0, 0, context.width, context.height);
+
+    for (var i = 0; i < colors.length; i++) {
+        drawRectangle(colors[i], context);
+        writeRectangle(colors[i], element + " .output")
+    }
+}
+
+function drawRectangle(rect, context) {
+    context.strokeStyle = rect.color;
+    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+}
+
+function writeRectangle(rect, element) {
+    $(element)
+        .append("<p>")
+        .append(rect.color + ": " + rect.width + "X" + rect.height)
+        .append(" @ " + rect.x + ":" + rect.y)
 }
 
 window.addEventListener("load", init);
